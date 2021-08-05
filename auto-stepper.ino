454d0b8f5e50aae35bf6aa82c6833c9148b6b2e3
Francesco Colasante <frank_997@hotmail.it>
2021-08-05 10:26:44 +0200

    changed stepper and delays, 1 prod

--- auto-stepper.ino
+++ auto-stepper.ino
@@ -1,69 +1,81 @@
-#include <Stepper.h>
+#include <AccelStepper.h>
+#define dirPin 3  // DIR+
+#define stepPin 4 // PUL+
+#define motorInterfaceType 1 //DRIVER MODE
+
+
 // PARAMETERS
-#define STEP_REVOLUTION 2048
-#define STEP_SINGLE 341
+#define STEP_REVOLUTION 800
+#define STEP_SINGLE 133
 
 /**
- * HP: 5 sec / giro => 12 rpm
+ * HP: step/sec 
  */
-#define STEP_SPEED 12
-
-#define DELAY_READ 500
-#define DELAY_CHECK 200
+#define STEP_SPEED STEP_REVOLUTION * 2 // 1600 step/sec => 2 giri/sec
+#define ACCELERATION 500 
+#define DELAY_READ 2000 // WAIT UNTIL READ
 #define INTERRUPT_PIN 2
-bool running = false;
-
+#define SENSOR_PIN 5
+volatile bool running = false;
 volatile bool start = false;
-Stepper myStepper(STEP_REVOLUTION, 4, 5, 6, 7);
+
+AccelStepper stepper = AccelStepper(motorInterfaceType, stepPin, dirPin);
 
 void setup() {
   Serial.begin(9600);
-  randomSeed(analogRead(0));
+  pinMode(SENSOR_PIN, INPUT);
   pinMode(INTERRUPT_PIN, INPUT_PULLUP);
+  delay(1000);
   attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), setStart, FALLING);
 
-  myStepper.setSpeed(STEP_SPEED);
+  stepper.setMaxSpeed(STEP_SPEED);
+  stepper.setAcceleration(ACCELERATION);
 }
 
 void loop() {
   if(start && !running){
     check();
   }
-  delay(DELAY_CHECK);
+  delay(500);
 }
 
 bool checkCorrectness(){
   delay(DELAY_READ);
-  int value = analogRead(0);
+  int value = digitalRead(SENSOR_PIN);
   Serial.print("read: "); Serial.println(value);
-  return value > 10;
+  return value == 1;
 }
 
 void moveMotor(int index){
-  // 0,1,2,3 => 341
-  // 4,5 => 342
+  // 0,1,2,3 => 133
+  // 4,5 => 134
   int currentStep = (index < 4) ? STEP_SINGLE : STEP_SINGLE + 1;
-  myStepper.step(currentStep);
+    // Set the target position:
+  stepper.move(currentStep);
+  // Run to target position with set speed and acceleration/deceleration:
+  stepper.runToPosition();
 }
 
 void check(){
+  start = false;
   if(running) return;
   running = true;
-  start = false;
   bool correct = true;
-  Serial.println("Checking...");
+  Serial.print("Checking...");
+  Serial.print(start); Serial.println(running);
   for(int i = 0; i < 6; i++){
     correct = checkCorrectness();
     if(!correct){
-      Serial.println("Incorrect measure");
+      Serial.println("Pezzo errato");
       break;
     }
     moveMotor(i);
   }
-  Serial.println("Check completed");
-  if(correct) Serial.println("Piece OK"); else Serial.println("Piece NC");
+  Serial.println("check completed");
+  if(correct) Serial.println("Pezzo OK"); else Serial.println("Pezzo NC");
   Serial.flush();
   running = false;
+  start = false;
 }
 
 void setStart(){
